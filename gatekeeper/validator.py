354f9c9d10aeac5fa19971ad59f80ca86212a7b1
Cletus Eze <ebukacletuseze@gmail.com>
2026-06-22 22:12:40 +0100

    Add structural validator using official Sigma CLI

--- gatekeeper/validator.py
+++ gatekeeper/validator.py
@@ -1,1 +1,19 @@
-print("validator.py works")
+import subprocess
+
+rule_path = "rules/modified/proc_creation_win_susp_whoami_as_param.yml"
+
+print("Running official Sigma validator on:", rule_path)
+print()
+
+result = subprocess.run(
+    ["sigma", "check", rule_path],
+    capture_output=True,
+    text=True
+)
+
+print(result.stdout)
+
+if result.returncode == 0:
+    print("[VALID] The rule passes official Sigma validation.")
+else:
+    print("[INVALID] The rule failed official Sigma validation.")
